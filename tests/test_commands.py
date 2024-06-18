3cc8c5a5b37b181bda06a14a13a7cbab39281c4e
Sebastian Markgraf <sebastian.markgraf@code-hive.de>
2024-06-18 22:38:03 +0200

    doc: Add test documentation to command tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,16 +2,22 @@ from whatsapp_transcribe.commands import Message, command_handling
 
 
 def test_echo():
+    """Test echo command --> Should return text prefixed with Echo:
+
+    e.g echo test --> Echo: test
+    """
     response = command_handling(Message(body="echo test", from_number="123"))
     assert response == "Echo: test"
 
 
 def test_help():
+    """Test invocation with help command --> Should return the help message"""
     response = command_handling(Message(body="help", from_number="123"))
     assert response.startswith("Send a voice message to transcribe and summarize.")
 
 
 def test_unknown():
+    """Test invocation without known command --> Should return the help message"""
     response = command_handling(Message(body="unknown", from_number="123"))
     # Expected response is the help message
     assert response == command_handling(Message(body="help", from_number="123"))
